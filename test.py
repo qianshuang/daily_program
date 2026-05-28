4d131ea81b2c68560badf7c0d557a5665a1ee0e9
QS <qs@192.168.2.10>
2026-05-28 17:42:04 +0800

    update

--- test.py
+++ test.py
@@ -4,38 +4,47 @@ from sentence_transformers import SentenceTransformer
 import time
 from utils import *
 
-batch_size = 32
+batch_size = 128
 model_path = "/apps/models/BAAI/bge-m3"
 model = SentenceTransformer(model_path, device="cuda", model_kwargs={"torch_dtype": "float16"}, local_files_only=True)
 model.encode(["init model"], batch_size=batch_size)
 print("Model loaded successfully, device: {}...".format(model.device))
 
-test_text = "That is a happy person, That is a happy person, That is a happy person, That is a happy person, That is a happy person, That is a happy person"
-print("Token length smaller: {}".format(count_token("That is a happy person")))
-print("Token length: {}".format(count_token(test_text)))
+# 验证超长是否截断
+print("model.max_seq_length: {}".format(model.max_seq_length))
+text = "你好吗？" * 100000
+print("Token cnt: {}".format(count_token(text)))
+embedding = model.encode(text)  # 超出8192不报错
+tokens = model.tokenizer.encode(text)
+print("自动截断后参与encode的Token数: {}".format(len(tokens)))
 
-sentences_1 = [test_text]
-sentences_10 = [test_text] * 10
-sentences_100 = [test_text] * 100
-sentences_1000 = [test_text] * 1000
-sentences_10000 = [test_text] * 10000
-
-start_1 = time.time()
-model.encode(sentences_1, batch_size=batch_size)
-print(f"Time cost for 1: {(time.time() - start_1) * 1000:.2f} ms")
-
-start_10 = time.time()
-model.encode(sentences_10, batch_size=batch_size)
-print(f"Time cost for 10: {(time.time() - start_10) * 1000:.2f} ms")
-
-start_100 = time.time()
-model.encode(sentences_100, batch_size=batch_size)
-print(f"Time cost for 100: {(time.time() - start_100) * 1000:.2f} ms")
-
-start_1000 = time.time()
-model.encode(sentences_1000, batch_size=batch_size)
-print(f"Time cost for 1000: {(time.time() - start_1000) * 1000:.2f} ms")
-
-start_10000 = time.time()
-model.encode(sentences_10000, batch_size=batch_size)
-print(f"Time cost for 10000: {(time.time() - start_10000) * 1000:.2f} ms")
+# 耗时测试
+# test_text = "That is a happy person, That is a happy person, That is a happy person, That is a happy person, That is a happy person, That is a happy person"
+# print("Token length smaller: {}".format(count_token("That is a happy person")))
+# print("Token length: {}".format(count_token(test_text)))
+#
+# sentences_1 = [test_text]
+# sentences_10 = [test_text] * 10
+# sentences_100 = [test_text] * 100
+# sentences_1000 = [test_text] * 1000
+# sentences_10000 = [test_text] * 10000
+#
+# start_1 = time.time()
+# model.encode(sentences_1, batch_size=batch_size)
+# print(f"Time cost for 1: {(time.time() - start_1) * 1000:.2f} ms")
+#
+# start_10 = time.time()
+# model.encode(sentences_10, batch_size=batch_size)
+# print(f"Time cost for 10: {(time.time() - start_10) * 1000:.2f} ms")
+#
+# start_100 = time.time()
+# model.encode(sentences_100, batch_size=batch_size)
+# print(f"Time cost for 100: {(time.time() - start_100) * 1000:.2f} ms")
+#
+# start_1000 = time.time()
+# model.encode(sentences_1000, batch_size=batch_size)
+# print(f"Time cost for 1000: {(time.time() - start_1000) * 1000:.2f} ms")
+#
+# start_10000 = time.time()
+# model.encode(sentences_10000, batch_size=batch_size)
+# print(f"Time cost for 10000: {(time.time() - start_10000) * 1000:.2f} ms")
