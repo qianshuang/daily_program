cd316d59f6c0d329d4c3b848bb36e3cd70bfb3fa
QS <qs@192.168.2.10>
2026-05-28 16:48:58 +0800

    update

--- test_web.py
+++ test_web.py
@@ -11,9 +11,8 @@ def do_post(api_url, query_body_):
     # print(json_res)
 
 
-query_api_url = "http://127.0.0.1:8000/chat"
+query_api_url = "http://127.0.0.1:8000/encode"
 query_body = {
-    "query": "对公付款申请，收款方银行怎么搜不到？",
-    "history": [{"type": "human", "data": "aaaaaaaaa"}]
+    "texts": ["你是谁？", "你好吗？"]
 }
 do_post(query_api_url, query_body)
